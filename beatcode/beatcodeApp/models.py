b2b442b9cf54df5470c65c544651bf38cba54ead
Madeleine Jones <mcj32@duke.edu>
2022-10-14 10:44:56 -0400

    added todo to models.py

--- beatcode/beatcodeApp/models.py
+++ beatcode/beatcodeApp/models.py
@@ -63,5 +63,8 @@ class InSet(models.Model):
     pid = models.ForeignKey(Problem)
     psid = models.ForeignKey(ProblemSet)   
 
-#to add later
-#class Task(models.Model):
+
+class ToDo(models.Model):
+    uid = models.ManyToManyField(User)
+    pid = models.ManyToManyField(Problem)
+    progress = models.TextField()
