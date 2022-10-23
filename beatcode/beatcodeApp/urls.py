9b633d323bf3db169f648307842076b8c28e3660
Sean Li <seanmtli@gmail.com>
2022-10-23 15:18:36 -0400

    fixed typo

--- beatcode/beatcodeApp/urls.py
+++ beatcode/beatcodeApp/urls.py
@@ -6,5 +6,5 @@ urlpatterns = [
     path('home/', Home.as_view(), name='home'),
     path('problem-set/<uuid:problem_set_id>/', ProblemSetView.as_view(), name='problem-set'),
     path('chart/', Chart.as_view(), name='chart'),
-    path('todos/',ToDo.as_view(), name='todos')
+    path('todos/',Todo.as_view(), name='todos')
 ]
