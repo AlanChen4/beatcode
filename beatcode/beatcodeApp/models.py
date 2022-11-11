5b6dcbfdcd1b7b5af4722f8193e69fd553764f35
Alan Chen <avchen4@gmail.com>
2022-11-11 03:28:41 -0500

    Add difficulty field to problem

--- beatcode/beatcodeApp/models.py
+++ beatcode/beatcodeApp/models.py
@@ -46,6 +46,7 @@ class Problem(models.Model):
     category = models.ManyToManyField(Category)
     name = models.CharField(max_length=255)
     problem_set = models.ManyToManyField(ProblemSet, blank=True)
+    difficulty = models.CharField(max_length=255, blank=True, null=True)
 
     def __str__(self):
         return self.name  
