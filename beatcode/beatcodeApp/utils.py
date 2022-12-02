f3e2ba97c03befc02deb98b8625c007a70fcfa1b
Alan Chen <avchen4@gmail.com>
2022-12-01 22:28:09 -0500

    Feature: Add util function to populate the database with thousands of data records

--- beatcode/beatcodeApp/utils.py
+++ beatcode/beatcodeApp/utils.py
@@ -1,6 +1,8 @@
+import uuid
 import time
 from datetime import datetime, timedelta
 from calendar import HTMLCalendar
+from authentication.models import CustomUser
 from .models import Category, Problem, Submission
 from scraper import get_problem_info
 
@@ -47,6 +49,42 @@ class Calendar(HTMLCalendar):
             cal += f'{self.formatweek(week, submissions)}\n'
         return cal
 
+
+def populate_dummy_data(number_of_accounts=100, number_of_submissions=100):
+    """
+    Populates the database with arbitrary number of accounts/submissions. Used to test scalability.
+    """
+    # generate accounts, use uuid4 for the username to avoid collisions
+    accounts = []
+    for _ in range(number_of_accounts):
+        account = CustomUser(
+            email=f'{uuid.uuid4()}@dummyemail.com',
+            first_name='fake_fname',
+            last_name='fake_lname',
+            password='password',
+            leetcode_username=f'fakeleetcodeusername-{uuid.uuid4()}',
+        )
+        accounts.append(account)
+    CustomUser.objects.bulk_create(accounts)
+
+    # generate submissions for each newly generate accounts
+    submissions = []
+    if len(Problem.objects.all()) <= 0:
+        raise Exception('Could not generate submissions since there are no valid problems in the database')
+    random_problem = Problem.objects.all()[0]
+    for account in accounts:
+        for _ in range(number_of_submissions):
+            submission = Submission(
+                user=account,
+                problem=random_problem,
+                sub_date=datetime.now(),
+                runtime=0,
+                mem_used=0,
+                success=False
+            )
+            submissions.append(submission)
+    Submission.objects.bulk_create(submissions)
+    
 
 def update_problems():
     """
