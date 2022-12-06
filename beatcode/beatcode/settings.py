e94da35f73633e1c17b9428c82485de9f1afb164
Alan Chen <avchen4@gmail.com>
2022-12-06 17:36:38 -0500

    chore: Add configuration for new production database hosted with planetscale

--- beatcode/beatcode/settings.py
+++ beatcode/beatcode/settings.py
@@ -103,6 +103,19 @@ DATABASES = {
     }
 }
 
+if os.environ.get('PRODUCTION_ENV') == 'HEROKU':
+    DATABASES = {
+    'default': {
+        'ENGINE': 'django.db.backends.mysql',
+        'NAME': os.environ.get('DB_NAME'),
+        'HOST': os.environ.get('DB_HOST'),
+        'PORT': os.environ.get('DB_PORT'),
+        'USER': os.environ.get('DB_USER'),
+        'PASSWORD': os.environ.get('DB_PASSWORD'),
+        'OPTIONS': {'ssl': {'ca': os.environ.get('MYSQL_ATTR_SSL_CA')}}
+    }
+    }
+
 
 # Password validation
 # https://docs.djangoproject.com/en/4.1/ref/settings/#auth-password-validators
