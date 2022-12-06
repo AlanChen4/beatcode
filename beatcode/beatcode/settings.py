4be4d966c0dff2ea814642c0448fcd0d7477f00a
Alan Chen <avchen4@gmail.com>
2022-12-06 17:58:28 -0500

    fix: Add mysqlclient to requirements.txt

--- beatcode/beatcode/settings.py
+++ beatcode/beatcode/settings.py
@@ -105,15 +105,15 @@ DATABASES = {
 
 if os.environ.get('PRODUCTION_ENV') == 'HEROKU':
     DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.mysql',
-        'NAME': os.environ.get('DB_NAME'),
-        'HOST': os.environ.get('DB_HOST'),
-        'PORT': os.environ.get('DB_PORT'),
-        'USER': os.environ.get('DB_USER'),
-        'PASSWORD': os.environ.get('DB_PASSWORD'),
-        'OPTIONS': {'ssl': {'ca': os.environ.get('MYSQL_ATTR_SSL_CA')}}
-    }
+        'default': {
+            'ENGINE': 'django.db.backends.mysql',
+            'NAME': os.environ.get('DB_NAME'),
+            'HOST': os.environ.get('DB_HOST'),
+            'PORT': os.environ.get('DB_PORT'),
+            'USER': os.environ.get('DB_USER'),
+            'PASSWORD': os.environ.get('DB_PASSWORD'),
+            'OPTIONS': {'ssl': {'ca': os.environ.get('MYSQL_ATTR_SSL_CA')}}
+        }
     }
 
 
